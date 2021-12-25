a58f70b86e7c0d630dfb6611dd5fc011da0347be
BeastCloud <96013887+BeastCloud@users.noreply.github.com>
2021-12-25 20:50:24 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -17,11 +17,8 @@ def list_buttons(update, context):
     except IndexError:
         return sendMessage('Send a search key along with command', context.bot, update)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Drive Root", f"types {user_id} root")
-    buttons.sbutton("Recursive", f"types {user_id} recu")
-    buttons.sbutton("Cancel", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
-    sendMarkup('Choose option to list.', context.bot, update, button)
+    sendMarkup('Choose Option to list.', context.bot, update, button)
 
 def select_type(update, context):
     query = update.callback_query
@@ -31,17 +28,13 @@ def select_type(update, context):
     data = query.data
     data = data.split(" ")
     if user_id != int(data[1]):
-        query.answer(text="Not Yours!", show_alert=True)
+        query.answer(text="😡Not Yours!", show_alert=True)
     elif data[2] in ["root", "recu"]:
         query.answer()
         buttons = button_build.ButtonMaker()
-        buttons.sbutton("Folders", f"types {user_id} folders {data[2]}")
-        buttons.sbutton("Files", f"types {user_id} files {data[2]}")
-        buttons.sbutton("Both", f"types {user_id} both {data[2]}")
-        buttons.sbutton("Cancel", f"types {user_id} cancel")
         button = InlineKeyboardMarkup(buttons.build_menu(2))
         editMessage('Choose option to list.', msg, button)
-    elif data[2] in ["files", "folders", "both"]:
+    elif data[2] in ["", "", ""]:
         query.answer()
         list_method = data[3]
         item_type = data[2]
@@ -60,7 +53,7 @@ def list_drive(key, bmsg, list_method, item_type):
     if button:
         editMessage(msg, bmsg, button)
     else:
-        editMessage(f'No result found for <i>{key}</i>', bmsg)
+        editMessage(f'No Result Found for <i>{key}</i>', bmsg)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_buttons, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
